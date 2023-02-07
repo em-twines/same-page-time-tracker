373c69c989f20dcf94280b5deab67e8284d16381
emilytwines <emdelbridge@gmail.com>
2023-02-07 10:46:07 -0500

    added name and date submitted to messages in manager mailbox

--- frontend/src/components/Manager/MailManager.jsx
+++ frontend/src/components/Manager/MailManager.jsx
@@ -13,18 +13,32 @@ export default function MailManager({
   const [mail, setMail] = useState([]);
   const [user, token] = useAuth();
   const [pendingRequests, setPendingRequests] = useState([]);
+  const [senders, setSenders] = useState([]);
+  const [combinedMessage, setCombinedMessage] = useState([]);
+
+  const merge = (arr1, arr2) =>
+    (arr1.length > arr2.length ? arr1 : arr2)
+      .map((_, i) => [arr1[i], arr2[i]])
+      .flat()
+      .filter(Boolean);
 
   function setMailbox() {
     let pending = [];
+    let senders = [];
     if (requests.length > 0) {
       requests.map((el) => {
         if (el.request_for_pto.is_pending === true) {
           pending.push(el.request_for_pto);
+          senders.push(el.user);
         }
-        return pending;
+        return pending, senders;
       });
-      setPendingRequests(pending);
+
+      // setPendingRequests(pending);
+      // setSenders(senders);
     }
+    let combined = merge(senders, pending);
+    setCombinedMessage(combined);
   }
 
   useEffect(() => {
@@ -43,33 +57,48 @@ export default function MailManager({
           height: 400,
           overflowY: "scroll",
         }}
-      
-      >  Inbox
-        {pendingRequests?.map((el) => {
-          //   return el.map((element) => {
+      >
+        {" "}
+        Inbox
+        <br></br>
+        <hr></hr>
+        
+
+        {combinedMessage?.map((el, i) => {
+
           return (
-            <Box
-              sx={{
-                bgcolor: "background.paper",
-                boxShadow: 1,
-                borderRadius: 2,
-                p: 2,
-                minWidth: 300,
-                margin: 2,
-              }}
-              key={el.id}
-            >
-              {" "}
-              {el.request_text}
-            </Box>
+            // <Box
+            //   sx={{
+            //     bgcolor: "background.paper",
+            //     boxShadow: 1,
+            //     borderRadius: 2,
+            //     p: 2,
+            //     minWidth: 300,
+            //     margin: 2,
+            //   }}
+            //   key={el.id}
+            // >
+              // {" "}
+              <div>
+                {i%2 === 0 ? (
+                  <div>
+                     <br></br>
+                  <p style = {{fontSize: '1rem'}}>{el.first_name} {el.last_name}</p>
+                  </div>
+                ) : ( 
+                  <div>
+                    <p style = {{fontSize: '.9rem'}}>{el.request_text}</p>
+                    <p style = {{fontSize: '.8rem'}}>{el.submission_time}</p>
+                    <br></br>
+                    <hr></hr>
+
+                  </div>
+                )}
+              </div>
+            // </Box>
           );
-          //   });
         })}
       </Box>{" "}
-      {/* <MessageManager
-        defaultMessage={defaultMessage}
-        setDefaultMesssage={setDefaultMesssage}
-      /> */}
     </div>
   );
 }
