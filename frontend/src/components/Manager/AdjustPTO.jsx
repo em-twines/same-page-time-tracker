be02b85dda6c706bd32a1c4e8740870a117775bf
emilytwines <emdelbridge@gmail.com>
2023-02-11 08:14:59 -0500

    restrutured Settings so that it auto-re-renders

--- frontend/src/components/Manager/AdjustPTO.jsx
+++ frontend/src/components/Manager/AdjustPTO.jsx
@@ -3,6 +3,7 @@ import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
+
 export default function AdjustPTO({ getAllEmployees, el }) {
   const [user, token] = useAuth();
 
--- frontend/src/pages/Manager/SettingsPage.jsx
+++ frontend/src/pages/Manager/SettingsPage.jsx
@@ -1,15 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import NewPTOSetting from '../../components/Manager/NewPTOSetting'
 import UpdatePTO from '../../components/Manager/UpdatePTO'
+import axios from "axios";
+import { toast } from "react-toastify";
+import useAuth from "../../hooks/useAuth";
+
 
 export default function SettingsPage() {
+
+  const [allPtoFrequenciesPerYear, setAllPtoFrequenciesPerYear] = useState([]);
+  const [allPtoHoursPerYear, setAllPtoHoursPerYear] = useState([]);
+  const [user, token] = useAuth();
+
+  useEffect(() => {
+    getAllPtoFrequenciesPerYear();
+    getAllPtoHoursPerYear();
+  }, []);
+
+  async function getAllPtoHoursPerYear() {
+    try {
+      let res = await axios.get(
+        `http://127.0.0.1:8000/api/requests_for_pto/manager/settings/hours/
+        `,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      setAllPtoHoursPerYear(res.data);
+    } catch (error) {
+      console.log(error);
+      toast(
+        "Sorry! We have encountered an error getting your pto hours allowance per year!"
+      );
+    }
+  }
+
+  async function getAllPtoFrequenciesPerYear() {
+    try {
+      let res = await axios.get(
+        `http://127.0.0.1:8000/api/requests_for_pto/manager/settings/frequencies/
+        `,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+
+      setAllPtoFrequenciesPerYear(res.data);
+    } catch (error) {
+      console.log(error);
+      toast(
+        "Sorry! We have encountered an error getting your pto rate allowance per year!"
+      );
+    }
+  }
+
+
+
+
+
 
 
 
   return (
     <div >
-        <UpdatePTO/>
-        <NewPTOSetting/>
+        <UpdatePTO allPtoFrequenciesPerYear = {allPtoFrequenciesPerYear} setAllPtoFrequenciesPerYear = {setAllPtoFrequenciesPerYear} allPtoHoursPerYear = {allPtoHoursPerYear} setAllPtoHoursPerYear = {setAllPtoHoursPerYear} />
+        <NewPTOSetting getAllPtoHoursPerYear = {getAllPtoHoursPerYear} getAllPtoFrequenciesPerYear = {getAllPtoFrequenciesPerYear}/>
     </div>
   )
 }
